3a14df3578352836551e0e07db9271f0025ade84
KabiruA159 <kaabdullahi12@gmail.com>
2022-12-11 02:32:20 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,13 +10,51 @@ int main(void)
 	char c = 'a';
 
 	while (c <= 'z')
-        {
+	{
 		putchar(c);
 		c++;
 	}
-	
+
 	putchar('\n');
 	return (0);
-}{
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 	
+
+
+
+
+
